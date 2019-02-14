bae1c2306ae1c2438ce47b3da1a11cf983180a5f
Lex Talionis <lextalionis1488@gmail.com>
2019-02-14 19:05:03 +0700

    Генератор классов: добавлены необходимые комментарии в генерируемый класс

--- ClassGenerator/Program.cs
+++ ClassGenerator/Program.cs
@@ -7,7 +7,7 @@ using LexTalionis.LexDbf.Common;
 
 namespace ClassGenerator
 {
-    class Program
+    static class Program
     {
         static void Main(string[] args)
         {
@@ -36,16 +36,20 @@ namespace ClassGenerator
 
             sb.AppendLine("using System;");
             sb.AppendLine("using LexTalionis.LexDbf.Common;");
+            sb.AppendLine();
+
             sb.AppendLine("// ReSharper disable CheckNamespace");
             sb.AppendLine("// ReSharper disable CSharpWarnings::CS1591");
             sb.AppendLine("// ReSharper disable InconsistentNaming");
-            
+            sb.AppendLine("// ReSharper disable ClassNeverInstantiated.Global");
+
             sb.AppendLine("/// <summary>");
             sb.AppendFormat("/// {0}", classname).AppendLine();
-            sb.AppendLine("/// <summary>");
+            sb.AppendLine("/// </summary>");
 
             sb.AppendFormat("public class {0}", classname).AppendLine();
 
+            sb.AppendLine("// ReSharper restore ClassNeverInstantiated.Global");
             sb.AppendLine("// ReSharper restore InconsistentNaming");
             sb.AppendLine("// ReSharper restore CSharpWarnings::CS1591");
             sb.AppendLine("// ReSharper restore CheckNamespace");
@@ -55,10 +59,10 @@ namespace ClassGenerator
             foreach (var col in columns)
             {
                 var type = ColumnInfo.GetTypeOfColumnS(col.Type);
-
+                
                 sb.AppendLine("\t/// <summary>");
                 sb.AppendFormat("\t/// {0}", col.Name).AppendLine();
-                sb.AppendLine("\t/// <summary>");
+                sb.AppendLine("\t/// </summary>");
 
                 sb.AppendFormat("\t[Field(Type = '{0}', Length = {1}, DecimalCount = {2})]",
                                 (char)col.Type, col.FieldLength,
@@ -66,11 +70,17 @@ namespace ClassGenerator
 
                 sb.AppendLine("\t// ReSharper disable CSharpWarnings::CS1591");
                 sb.AppendLine("\t// ReSharper disable InconsistentNaming");
+                sb.AppendLine("\t// ReSharper disable RedundantNameQualifier");
+                sb.AppendLine("\t// ReSharper disable UnassignedField.Global");
 
                 sb.AppendFormat("\tpublic {0} {1};", type, col.Name).AppendLine();
 
+                sb.AppendLine("\t// ReSharper restore UnassignedField.Global");
+                sb.AppendLine("\t// ReSharper restore RedundantNameQualifier");
                 sb.AppendLine("\t// ReSharper restore InconsistentNaming");
                 sb.AppendLine("\t// ReSharper restore CSharpWarnings::CS1591");
+                
+                sb.AppendLine();
             }
 
             sb.AppendLine("}");
